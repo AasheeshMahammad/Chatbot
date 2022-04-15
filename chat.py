995a8933c0bdffba45ee89c30916f03f36aab3b5
Aasheesh Mahammad <54064611+AasheeshMahammad@users.noreply.github.com>
2022-04-15 14:06:36 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -4,7 +4,7 @@ from utils import bag_of_words, tokenize
 
 
 device = torch.device('cuda' if torch.cuda.is_available()  else 'cpu')
-with open("intents.json",'r') as f:
+with open("questions.json",'r') as f:
     intents = json.load(f)
 
 file = "data.pth"
